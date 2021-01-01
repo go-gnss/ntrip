120c8639ab7475827bfd6d4e5f4cbdcf26d4e8aa
Brandon <brandon.owen@hotmail.com>
2021-01-01 18:23:35 +1100

    Fix relay cmd

--- cmd/relay/relay.go
+++ cmd/relay/relay.go
@@ -12,19 +12,21 @@ import (
 	"github.com/go-gnss/ntrip"
 )
 
+var (
+	reader, writer = io.Pipe()
+)
+
 func main() {
-	source := flag.String("source", "", "Source NTRIP caster mountpoint to stream from")
+	source := flag.String("source", "", "Source NTRIP caster URL to stream from")
 	sourceUsername := flag.String("suser", "", "Username for accessing the Source NTRIP caster")
 	sourcePassword := flag.String("spass", "", "Password for accessing the Source NTRIP caster")
-	destination := flag.String("destination", "", "NTRIP caster mountpoint to stream from")
+	destination := flag.String("dest", "", "NTRIP caster URL to stream from")
 	destUsername := flag.String("duser", "", "Username for accessing the Destination NTRIP caster")
 	destPassword := flag.String("dpass", "", "Password for accessing the Destination NTRIP caster")
 	timeout := flag.Duration("timeout", 2, "NTRIP reconnect timeout")
 	flag.Parse()
 
-	r, w := io.Pipe()
-	// Serve whatever is written to the PipeWriter
-	go serve(*destination, *destUsername, *destPassword, *timeout, r)
+	go serve(*destination, *destUsername, *destPassword, *timeout)
 
 	// Write response body to PipeWriter
 	client, _ := ntrip.NewClientRequestV2(*source)
@@ -40,26 +42,26 @@ func main() {
 		data := make([]byte, 4096)
 		br, err := resp.Body.Read(data)
 		for ; err == nil; br, err = resp.Body.Read(data) {
-			w.Write(data[:br])
+			writer.Write(data[:br])
 		}
 
 		fmt.Println("client connection died", err)
 	}
 }
 
-func serve(url, username, password string, timeout time.Duration, body io.ReadCloser) {
-	req, _ := ntrip.NewServerRequestV2(url, body)
-	req.SetBasicAuth(username, password)
-	go func() {
-		for ; ; time.Sleep(time.Second * timeout) {
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil || resp.StatusCode != 200 {
-				fmt.Println("server failed to connect", resp, err)
-				continue
-			}
-			fmt.Println("server connected")
-			ioutil.ReadAll(resp.Body)
-			fmt.Println("server connection died")
+// Serve whatever is written to the PipeWriter
+func serve(url, username, password string, timeout time.Duration) {
+	for ; ; time.Sleep(time.Second * timeout) {
+		reader, writer = io.Pipe()
+		req, _ := ntrip.NewServerRequestV2(url, reader)
+		req.SetBasicAuth(username, password)
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil || resp.StatusCode != 200 {
+			fmt.Println("server failed to connect", resp, err)
+			continue
 		}
-	}()
+		fmt.Println("server connected")
+		ioutil.ReadAll(resp.Body)
+		fmt.Println("server connection died")
+	}
 }
